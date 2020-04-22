b3c338a3429ad46e395e3b187bb491c82f07de4a
alegranados03 <alegranados03@gmail.com>
2020-04-21 20:44:42 -0600

    start point

---
+++ src/components/VoiceMailList/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Table from './Table';
+class index extends Component {
+    render() {
+        return (
+            <div>
+                <Table></Table>
+            </div>
+        );
+    }
+}
+
+export default index;
